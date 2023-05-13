b7766cfbea2be4a368cc8d498ac1ceb1d059376e
sajad <sajadmail3@gmail.com>
2023-05-13 10:21:34 +0330

    test5

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,12 @@
+  //tolbarcode
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
+  });
+  //menue code
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'left'
+    });
   });
